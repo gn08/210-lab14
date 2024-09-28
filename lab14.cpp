09ecd1f40f0d563df2026b2538326ee6d3eb68ec
gn08 <gabbyfilipova@gmail.com>
2024-09-28 02:01:07 -0700

    commit 2:00

--- lab14.cpp
+++ lab14.cpp
@@ -1,8 +1,24 @@
+// COMSC-210 | Lab 14 | Gaby Filipova
 #include <iostream>
 using namespace std;
 
-int main(){
+class Color{
+    private:
+        int red;
+        int green;
+        int blue;
 
-    
+    public:
+        void red(){
+            red = 
+        }
+
+        void green(){
+
+        }
+
+        void blue(){
+
+        }
 }
 
