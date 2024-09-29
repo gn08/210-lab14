137d65ffd09f8a533600e9fea0af01676a876312
gn08 <gabbyfilipova@gmail.com>
2024-09-29 01:04:26 -0700

    commit 1:05

--- lab14.cpp
+++ lab14.cpp
@@ -46,19 +46,23 @@ class Color{
         void set_blue(int b){
             blue = b;
         }
-
+    // get_red() gets red part of color
+    // return: integer value for red
         int get_red() const{
             return red;
         }
-
+    // get_green() gets green part of color
+    // return: integer value for green
         int get_green() const{
             return green;
         }
-
+    // get_blue() gets blue part of color
+    // return: integer value for blue
         int get_blue() const{
             return blue;
         }
-
+    // print() prints red, green, and blue values
+    // return: void
         void print () const{
             cout << "Red: " << red << endl
                 << "Green: " << green << endl 
@@ -67,12 +71,13 @@ class Color{
 };
 
 int main(){
+    //color object: color
     Color color;
-
+    //set color values
     color.set_red(50);
     color.set_green(100);
     color.set_blue(200);
-
+    //output color values
     cout << "Red Value: " << color.get_red() << endl;
     cout << "Green Value: " << color.get_green() << endl;
     cout << "Blue Value: " << color.get_blue() << endl;
